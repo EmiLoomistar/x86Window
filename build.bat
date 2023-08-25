0390bc145bd0c1f483cafedb830fad0779d8d81e
EmiLoomistar <36137649+EmiLoomistar@users.noreply.github.com>
2023-08-25 12:49:17 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
 
-as.exe %1.asm -o %1.o
-gcc.exe %1.o -o %1.exe libraylib.a -lopengl32 -lgdi32 -lwinmm
-if %errorlevel%==0 (%1.exe)
-del %1.o
+as.exe game.asm -o game.o
+gcc.exe game.o libraylib.a -lopengl32 -lgdi32 -lwinmm -o game.exe
+del game.o
+game.exe
+
 
